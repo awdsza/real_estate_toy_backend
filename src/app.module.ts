feb309e7471b6fcb60fde3de6c928f5ff41aa005
awdsza <37355034+awdsza@users.noreply.github.com>
2023-05-18 01:46:25 +0900

    [feat] init Project

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TradeModule } from './trade/trade.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { ApartmentModule } from './apartment/apartment.module';
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+      ? '.stage.env'
+      : '.development.env',
+  ),
+});
+@Module({
+  imports: [
+    TradeModule,
+    ApartmentModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'estate',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    }),
+  ],
+})
+export class AppModule {}
